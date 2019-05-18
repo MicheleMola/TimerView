16a152ed3ab28aefbadc79e17c6a052ba85453b2
Michele Mola <michele.mola92@gmail.com>
2019-05-18 15:08:47 +0200

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Michele Mola on 18/05/2019.
+//  Copyright © 2019 Michele Mola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
