0e993e63f1194b7c65ee13e76afc6bda998a093b
Michele Mola <michele.mola92@gmail.com>
2019-05-22 12:06:29 +0200

    added sequence

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var timerView: TimerView!
   
+  @IBOutlet weak var cameraImageView: UIImageView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -19,10 +21,29 @@ class ViewController: UIViewController {
     self.timerView.lineWidth = 6
     self.timerView.duration = 10
     self.timerView.timerFontSize = 20
+    
+    setup()
   }
   
   @IBAction func playPressed(_ sender: UIButton) {
     timerView.startAnimation()
+  }
+  
+  func setup() {
+    var images: [UIImage] = []
+    
+    for i in 1...186 {
+      print("here")
+      let bundlePath = Bundle.main.path(forResource: "Camera_\(i)", ofType: "png")!
+      let image = UIImage(contentsOfFile: bundlePath)!
+      images.append(image)
+    }
+//    let images = (1...186).map { UIImage(named: "Camera_\($0)")! }
+    
+    self.cameraImageView.animationImages = images
+    self.cameraImageView.animationDuration = 5
+    self.cameraImageView.animationRepeatCount = 1
+    self.cameraImageView.startAnimating()
   }
   
 
